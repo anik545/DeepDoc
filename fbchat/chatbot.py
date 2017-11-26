25cd1bc56f400b409c26c5b95d7eca467b2109bd
VKuosma <vibe96@gmail.com>
2017-11-26 06:31:00 +0000

    Added pain time -feature
    The feature makes the doctor ask how long a person has had the pain after they tell how bad the pain is. This value is not parsed. The json's structure is changed slightly.

--- fbchat/chatbot.py
+++ fbchat/chatbot.py
@@ -2,7 +2,6 @@ from fbchat import Client, log
 from fbchat.models import *
 import json
 
-
 class EchoBot(Client):
     def __init__(self, username, password):
         super(EchoBot, self).__init__(username, password)
@@ -11,17 +10,19 @@ class EchoBot(Client):
         self.list_= ["chest", "head", "shoulders", "knee", "toe", "heart",
             "stomach", "eye", "back", "arm"]
 
-
-
+    #onMessage is called every time a message is detected.
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
         self.markAsDelivered(author_id, thread_id)
         self.markAsRead(author_id)
 
+        #This is a FSA design, where action on message varies based on the automata's internal state
+        #Initial state, look for a greeting
         if self.state == 0 and author_id != self.uid:
             self.thread_id = thread_id
             self.send(Message(text = "Hello, welcome to deepdoc. What is your name?"), thread_id=thread_id, thread_type=thread_type)
             self.state +=1
 
+        #Look for a name
         elif self.state == 1 and author_id != self.uid and self.thread_id==thread_id:
             log.info("{} from {} in {}".format(message_object, thread_id, thread_type.name))
             self.send(Message(text ="Your name is " + message_object.text +". My name is chatbot.\nWhat's the matter?"), thread_id=thread_id, thread_type=thread_type)
@@ -30,6 +31,7 @@ class EchoBot(Client):
             log.info("name of person= " + self.name)
             self.state +=1
 
+        #Look for a type of pain. If one is not given, end session
         elif self.state == 2 and author_id != self.uid and self.thread_id==thread_id:
             matter = message_object.text
             log.info("matter of person= " + matter)
@@ -49,13 +51,14 @@ class EchoBot(Client):
                     self.state += 1
                     self.list_.remove(word)
             if not isFound:
-                self.state = 4
+                self.state = 5
                 self.send(Message(text = "Sorry we dont know about your pain point"), thread_id=thread_id, thread_type=thread_type)
                 # Make json file of what data was inputted by user
                 dic = {'name' : self.name, 'pains' : self.painlist}
                 jsonFile = json.dumps(dic)
                 log.info(jsonFile)
 
+        #If pain was given, look for scale of pain (0-10)
         elif self.state == 3 and author_id != self.uid and self.thread_id==thread_id:
             try:
                 scale = int(message_object.text)
@@ -63,12 +66,20 @@ class EchoBot(Client):
                 if scale <0 or scale >10:
                     self.send(Message(text = "This is not a number. Try again."), thread_id=thread_id, thread_type=thread_type)
                 else:
-                    self.painlist.append({self.pain: scale})
-                    self.state = 2
-                    self.send(Message(text = "What other pain are you experiencing?"), thread_id=thread_id, thread_type=thread_type)
-
+                    self.scale = scale
+                    self.state = 4
+                    self.send(Message(text = "How long can you say you've had this pain?"), thread_id=thread_id, thread_type=thread_type)
             except ValueError:
                 self.send(Message(text = "This is not a number. Try again."), thread_id=thread_id, thread_type=thread_type)
+                    
+        #If pain and scale were given, look for how long the pain has been happening (just natural language)
+        elif self.state == 4 and author_id != self.uid and self.thread_id==thread_id:
+            painTime = int(message_object.text)
+            log.info(painTime)
+            self.painlist.append({'pain' : self.pain, 'scale' : self.scale, 'time' : painTime})
+            self.state = 2
+            self.send(Message(text = "What other pain are you experiencing?"), thread_id=thread_id, thread_type=thread_type)
+            
         elif author_id != self.uid or author_id != self.clientId:
             self.send(Message(text = "The doctor is busy and talking to someone else. Try again in 5 minuites."), thread_id=thread_id, thread_type=thread_type)
 
